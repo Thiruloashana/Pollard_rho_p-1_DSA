2a62241bcc23a70b4c4a08356af36932987bc3d8
Thiruloashana A <105217007+Thiruloashana@users.noreply.github.com>
2024-01-04 21:02:23 +0530

    Update final.py

--- final.py
+++ final.py
@@ -11,7 +11,7 @@ global e2_entry
 
 # Pollard Rho Factorization window
 def rho():
-    #function to go back to the main menu
+
     def go_back_to_menu():
         window.destroy()
         main_menu()
@@ -34,32 +34,31 @@ def rho():
         else:
             return d, values
 
-    # Calculate the greatest common divisor (GCD)
+    # GCD
     def gcd(a, b):
         while b:
             a, b = b, a % b
         return a
 
-    # Function to perform factorization
+    # Factorization
     def factorize():
-        n = int(entry.get())  # Get the input number from the entry field
+        n = int(entry.get())  # user input from the entry field
         B = 100  # Maximum number of iterations
-        factor, values = pollard_rho(n, B)  # Call the Pollard Rho algorithm
+        factor, values = pollard_rho(n, B) 
 
         if factor is None:
             result_label.config(text="Could not factorize further.")
         else:
             result_label.config(text=f"Factors: {factor}")
 
-        # Display the intermediate values in the GUI
         values_text.config(text="Iteration   |   f(x)   |   f(f(x))   |   c   |   d\n" + "\n".join(
             f"{i+1:10} | {x:8} | {y:11} | {c:5} | {d:5}" for i, (x, y, c, d) in enumerate(values)))
 
-    # main tkinter window
+    # GUI
     window = tk.Tk()
     window.title("Pollard Rho Factorization")
 
-    # Create and place GUI elements on the window
+
     label = ttk.Label(window, text="Enter a number to factorize:")
     label.grid(row=0, column=0)
 
@@ -75,19 +74,17 @@ def rho():
     values_text = ttk.Label(window, text="")
     values_text.grid(row=3, column=0, columnspan=2)
 
-    # Create a Back button to return to the menu
     back_button = ttk.Button(
         window, text="Back to Menu", command=go_back_to_menu)
     back_button.grid(row=4, column=0, columnspan=2)
 
-    # Start the main GUI event loop
+    # Start GUI
     window.mainloop()
 
 
-# Define a function to create the Pollard's p-1 Factorization window
+# Pollard's p-1 Factorization
 def p_1():
 
-    # Define a function to go back to the main menu
     def go_back_to_menu():
         window.destroy()
         main_menu()
@@ -109,28 +106,25 @@ def p_1():
             p += 1
         return factors
 
-    # Calculate the greatest common divisor (GCD)
     def gcd(a, b):
         while b:
             a, b = b, a % b
         return a
 
-    # Function to perform factorization
     def factorize():
-        n = int(entry.get())  # Get the input number from the entry field
+        n = int(entry.get()) 
         B = 100  # Maximum number of iterations
-        factors = pollard_p_minus_1(n, B)  # Call the Pollard's p-1 algorithm
+        factors = pollard_p_minus_1(n, B)  
 
         if len(factors) == 0:
             result_label.config(text="Could not factorize further.")
         else:
             result_label.config(text="Factors: " + " ".join(map(str, factors)))
 
-    # Create the main tkinter window
+
     window = tk.Tk()
     window.title("Pollard's p-1 Factorization")
 
-    # Create and place GUI elements on the window
     label = ttk.Label(window, text="Enter a number to factorize:")
     label.grid(row=0, column=0)
 
@@ -143,46 +137,41 @@ def p_1():
     result_label = ttk.Label(window, text="")
     result_label.grid(row=2, column=0, columnspan=2)
 
-    # Create a Back button to return to the menu
     back_button = ttk.Button(
         window, text="Back to Menu", command=go_back_to_menu)
     back_button.grid(row=4, column=0, columnspan=2)
 
-    # Start the main GUI event loop
     window.mainloop()
 
-
-
-# Function to calculate the multiplicative inverse of r modulo q
 def calc_inv(r, q):
-    for i in range(1, q):  # Iterate through numbers 1 to q
-        if (r * i) % q == 1:  # Check if (r * i) modulo q is equal to 1
-            return i  # Return i as the multiplicative inverse of r modulo q
+    for i in range(1, q):
+        if (r * i) % q == 1:  
+            return i  
 
 
 
-# Function to create the DSA implementation window
+#  DSA implementation 
 def dsa():
-    # Function to go back to the main menu
-    def go_back_to_menu():
-        window.destroy()  # Close the current DSA window
-        main_menu()  # Return to the main menu
 
-    # Function to generate DSA key pair
+    def go_back_to_menu():
+        window.destroy()  
+        main_menu()  
+
+    #  generate DSA key pair
     def generate_key():
-        global e2_entry  # Access e2_entry as a global variable
+        global e2_entry  
         p = int(p_entry.get())  
         q = int(q_entry.get())  
         d = int(d_entry.get())  
-        e0 = random.randint(2, p - 2)  # Generate a random e0
-        e1 = int(e1_entry.get())  # Get the value of e1 from the input field
-        e2 = e1 ** d  # Calculate e2
-        e2 = e2 % p  # Take e2 modulo p
-        public_key.set(f"Public Key: (e1={e1}, e2={e2}, p={p}, q={q})")  # Set the public key label
-        private_key.set(f"Private Key: d={d}")  # Set the private key label
-        e2_entry = e2  # Set e2_entry to the calculated e2
+        e0 = random.randint(2, p - 2)  
+        e1 = int(e1_entry.get())  
+        e2 = e1 ** d
+        e2 = e2 % p  
+        public_key.set(f"Public Key: (e1={e1}, e2={e2}, p={p}, q={q})")
+        private_key.set(f"Private Key: d={d}") 
+        e2_entry = e2 
 
-    # Function to sign a message using DSA        
+    # sign a message using DSA        
     def sign():
         global e2_entry, s1_entry, s2_entry
         p = int(p_entry.get())
@@ -193,30 +182,30 @@ def dsa():
         h_m = int(h_m_entry.get())
 
         s1 = (pow(e1, r) % p) % q
-        r_inv = calc_inv(r, q) # Calculate the multiplicative inverse of r
+        r_inv = calc_inv(r, q) # multiplicative inverse of r
         print("k_inv", r_inv)
         s2 = ((h_m + (d * s1)) * r_inv) % q # Calculate s2
         print("s2", s2)
         signature.set(f"Signature: (s1={s1}, s2={s2})")
-        s1_entry = s1  # Set s1_entry to the calculated s1
-        s2_entry = s2  # Set s2_entry to the calculated s2
+        s1_entry = s1  
+        s2_entry = s2 
 
 
 
-    # Function to verify a DSA signature
+    # verify a DSA signature
     def verify():
-        global e2_entry, s1_entry, s2_entry  # Access these variables as global
+        global e2_entry, s1_entry, s2_entry 
         e1 = int(e1_entry.get())  
-        e2 = e2_entry  # Access the value of e2 as a global variable
-        s1 = s1_entry  # Access the value of s1 as a global variable
+        e2 = e2_entry  
+        s1 = s1_entry  
         p = int(p_entry.get())  
         q = int(q_entry.get())  
-        s2 = s2_entry  # Access the value of s2 as a global variable
+        s2 = s2_entry
         h_m = int(h_m_entry.get())  
         v = ((pow(e1, (h_m * calc_inv(s2, q)) % q) *
-             pow(e2, (s1 * calc_inv(s2, q)) % q)) % p) % q  # Calculate v
-        if 0 < s1 < q and 0 < s2 < q:  # Check if s1 and s2 are within valid ranges
-            if v == s1:  # Check if v matches s1
+             pow(e2, (s1 * calc_inv(s2, q)) % q)) % p) % q  
+        if 0 < s1 < q and 0 < s2 < q:  
+            if v == s1:  
                 output_text.set("Signature verified successfully.") 
             else:
                 output_text.set("Signature verification failed.")  
@@ -258,7 +247,6 @@ def dsa():
     h_m_entry = tk.Entry(window)
     h_m_entry.pack()
 
-    # Create and place buttons
     keygen_button = tk.Button(
         window, text="Generate Key Pair", command=generate_key)
     keygen_button.pack()
@@ -269,7 +257,6 @@ def dsa():
     verify_button = tk.Button(window, text="Verify", command=verify)
     verify_button.pack()
 
-    # Create and place output labels
     public_key = tk.StringVar()
     private_key = tk.StringVar()
     signature = tk.StringVar()
